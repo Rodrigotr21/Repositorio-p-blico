a052c01e26164178b09475ebaad2ce6dbae5974f
Rodrigotr21 <159547998+Rodrigotr21@users.noreply.github.com>
2025-12-07 07:46:00 -0500

    Update Consultas_Básicas.sql

--- Consultas_Básicas.sql
+++ Consultas_Básicas.sql
@@ -1,3 +1,5 @@
+-- CONSULTAS BÁSICAS PARA SQL
+-- BY: Rodrigo Yauli
 SELECT * -- Para llamar todos los datos
 FROM BASE_COMPLETA -- De una determinada base
 --------------------------------------------
@@ -5,8 +7,15 @@ SELECT ColumnaA -- Para seleccionar una/unas columnas en específico
 FROM BASE_COMPLETA -- De una determinada base
 --------------------------------------------
 -- ¿Si debo filtrar ciertas columnas con algún filtro?
--- Depende de que tipo de filtro estemos hablando, por ejemplo si no es de tipo numérico entonces:
+-- Depende de que tipo de filtro estemos hablando
+-- por ejemplo si no es de tipo numérico entonces:
 SELECT ColumnaA, ColumnaB
 FROM BASE_COMPLETA
 WHERE ColumnaA = 'Av Siempre viva'
+ORDER BY ColumnaA, ColumnaB
+-- por ejemplo si es de tipo numérico entonces:
+SELECT ColumnaA, ColumnaB, COUNT(ColumnaC) AS TotalC
+FROM BASE_COMPLETA
+GROUP BY ColumnaA, ColumnaB
+HAVING TotalC > 10
 ORDER BY ColumnaA, ColumnaB
